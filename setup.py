2a0de46e14ddbb51bc5fe8a4d95899f54e2a8b10
Taiki Tajiri <tat.j.kikyo@gmail.com>
2017-06-21 20:42:59 +0900

    reconstruct as a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='mbottle',
+        packages=['mbottle'],
+        include_package_data=True,
+        install_requires=[
+            'flask',
+            'rauth',
+            ],
+        )
