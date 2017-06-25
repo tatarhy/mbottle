c5df1e08b5f2c9b69cc5674ef40ea4def9191b4a
Taiki Tajiri <tat.j.kikyo@gmail.com>
2017-06-25 12:10:32 +0900

    reformat

--- mbottle/mbottle.py
+++ mbottle/mbottle.py
@@ -7,21 +7,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['MBOTTLE_SECRET_KEY']
 
 zaim = OAuth1Service(
-        name='zaimtestt1',
-        consumer_key=os.environ['ZAIM_CONSUMER_KEY'],
-        consumer_secret=os.environ['ZAIM_CONSUMER_SECRET'],
-        request_token_url='https://api.zaim.net/v2/auth/request',
-        access_token_url='https://api.zaim.net/v2/auth/access',
-        authorize_url='https://auth.zaim.net/users/auth',
-        base_url='https://api.zaim.net/v2')
+    name='zaimtestt1',
+    consumer_key=os.environ['ZAIM_CONSUMER_KEY'],
+    consumer_secret=os.environ['ZAIM_CONSUMER_SECRET'],
+    request_token_url='https://api.zaim.net/v2/auth/request',
+    access_token_url='https://api.zaim.net/v2/auth/access',
+    authorize_url='https://auth.zaim.net/users/auth',
+    base_url='https://api.zaim.net/v2')
+
 
 @app.route("/")
 def root():
-    request_token, request_token_secret = zaim.get_request_token(params={'oauth_callback':'http://localhost:5000/callback/zaim'})
+    request_token, request_token_secret = zaim.get_request_token(
+        params={'oauth_callback': 'http://localhost:5000/callback/zaim'})
     session['request_token'] = request_token
     session['request_token_secret'] = request_token_secret
     zaim_auth_url = zaim.get_authorize_url(request_token)
     return render_template('connect.html', zaim_auth_url=zaim_auth_url)
+
 
 @app.route("/callback/zaim")
 def callback_zaim():
@@ -37,6 +40,7 @@ def callback_zaim():
 
     return ''
 
+
 @app.route("/test")
 def test():
     auth_session = zaim.get_session((session['access_token'], session['access_token_secret']))
@@ -44,7 +48,7 @@ def test():
     r = auth_session.get('https://api.zaim.net/v2/category', params={'format': 'json'})
     print(r.json())
 
-    #return r.json()
+    # return r.json()
     return ''
 
 
